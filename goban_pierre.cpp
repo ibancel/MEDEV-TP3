ade182799643e2cf7196974bda8107e8573bb123
lauriane-legoff <lauriane.le-goff@eleves.ec-nantes.fr>
2015-12-03 12:01:46 +0100

    Méthodes pour Goban et groupe

---
+++ goban_pierre.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include "Go.h"
+
+
+void Goban :: affichage(){
+
+    int i,j;
+
+    for (i=0; i<taille;i++){
+        for (j=0; j<taille;j++){
+            cout<<Goban[i][j]->get_couleur()<<\t;
+
+        }
+        cout<<endl;
+    }
+
+
+}
+
+
+Goban::Goban(int t){
+
+    taille=t;
+
+    for (i=0; i<taille;i++){
+        for (j=0; j<taille;j++){
+            Goban[i][j]=new Groupe(0);
+
+        }
+
+    }
+
+
+}
