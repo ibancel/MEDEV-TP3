775c0439342e14a4e269823220359307a0465a37
lauriane-legoff <lauriane.le-goff@eleves.ec-nantes.fr>
2015-12-03 11:04:03 +0100

    added Class headers

---
+++ Go.h
@@ -0,0 +1,61 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+
+
+class Goban {
+
+private:
+
+    int taille;
+    vector <vector<Pierres*> > plateau;
+
+public :
+
+    int get_taille(){return taille;};
+    void affichage ();
+    Goban (int t);
+
+};
+
+
+class Groupe {
+
+
+private :
+
+    int couleur; //0 vide; 1 blanc, 2 noir
+    int nb_liberte;
+    vector <coord> pierres;
+
+public:
+
+    int get_couleur () {return couleur;};
+    int get_nb_liberte(){return nb_liberte;};
+    vector <coord> get_pierres(){return pierres;}
+
+};
+
+
+
+class Joueur {
+    private:
+        int couleur;
+        int nb_captures;
+        bool passe;
+
+    public:
+        void passer();
+        void poser();
+
+        int get_couleur(){return couleur;};
+        int get_nb_captures(){return nb_capture;};
+        bool get-passe(){return passe;};
+
+        Joueur(int c);
+
+
+
+}
+
+
