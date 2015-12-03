960c2eac0533610c682f70a847da002e5bd708ad
Togandel <adrien.vinel@free.fr>
2015-12-03 12:19:34 +0100

    création des methodes Attention poser incomplete non testé

---
+++ joueur.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "gtest/gtest.h"
+#include "Go.h"
+
+using namespace std;
+
+void Joueur::passer(){//permet de savoir que le joueur passe
+    passe = true;
+}
+
+Joueur::Joueur(int c){//initialisation d'un joueur
+    couleur = c;
+    nb_captures=0;
+    passe=false;
+}
+
+/*void poser(vector<vector<Groupe*> > plateau, int c){
+    int i,j;//coordonnée de pose
+
+    cout<<"Entrer les coordonnees ou vous voulez poser la pierre"<<endl;
+    cout<< "x =";
+    cin>>i;
+    cout<<endl<<"y =";
+    cin>>j;
+    cout<<endl;
+
+    if (plateau[i][j]->get_couleur() ==0){//la case est vide
+        plateau[i][j]=(new )
+
+
+    }
+
+
+}*/
