2091ea0eb68f0e5a7225d13adfd45ea5b719d3c4
ibancel <iold.bancel@eleves.ec-nantes.fr>
2015-12-10 08:49:59 +0100

    Ajout de tests pour l'initialisation du plateau

--- main.cpp
+++ main.cpp
@@ -11,3 +11,35 @@ int main(int argc, char **argv)
 
     return RUN_ALL_TESTS();
 }
+
+TEST(initialisation_test, plateau5_vide)
+{
+	Goban go = new Goban(5);
+	EXPECT_EQ(5, go->get_taille());
+	vector< vector<Groupe*> > plateau = go->get_plateau();
+
+	for(int i = 0 ; i < 5 ; i++)
+	{
+		for(int j = 0 ; j < 5 ; j++)
+		{
+			SCOPED_TRACE(i << "x" << j);
+			EXPECT_EQ(0, plateau[i][j].size());
+		}
+	}
+}
+
+TEST(initialisation_test, plateau9_vide)
+{
+	Goban go = new Goban(9);
+	EXPECT_EQ(9, go->get_taille());
+	vector< vector<Groupe*> > plateau = go->get_plateau();
+
+	for(int i = 0 ; i < 9 ; i++)
+	{
+		for(int j = 0 ; j < 9 ; j++)
+		{
+			SCOPED_TRACE(i << "x" << j);
+			EXPECT_EQ(0, plateau[i][j].size());
+		}
+	}
+}
